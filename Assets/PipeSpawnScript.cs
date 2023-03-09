d564a33843f964205e348844609b4de2da840d6d
Mateo Wallace <mateo.t.wallace@gmail.com>
2023-03-09 17:11:58 -0500

    pipes y position is randomized on spawn

--- Assets/PipeSpawnScript.cs
+++ Assets/PipeSpawnScript.cs
@@ -8,6 +8,7 @@ public class PipeSpawnScript : MonoBehaviour
     public GameObject pipe;
     public float spawnRate = 2;
     private float timer = 0;
+    public float heightOffset = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -32,6 +33,9 @@ public class PipeSpawnScript : MonoBehaviour
 
     void spawnPipe()
     {
-        Instantiate(pipe, transform.position, transform.rotation);
+        float lowestPoint = transform.position.y - heightOffset;
+        float highestPoint = transform.position.y + heightOffset;
+
+        Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestPoint, highestPoint), 0), transform.rotation);
     }
 }
